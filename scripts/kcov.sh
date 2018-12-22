4c53af3333eb960e88e667f6f1046aea2ced37eb
Thomas Schaller <torkleyy@gmail.com>
2018-12-22 16:09:11 +0000

    Upload coverage report to codecov

--- scripts/kcov.sh
+++ scripts/kcov.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Create Cargo.lock
-cargo check --all || { echo "Not inside project root"; exit 1; }
-
 # Run image (non-blocking)
 docker run -dt --security-opt seccomp=unconfined --name mykcov1 --entrypoint /bin/sh registry.gitlab.com/torkleyy/docker-cargo-kcov || exit 1
 
@@ -10,7 +7,8 @@ docker cp Cargo.lock mykcov1:/volume
 docker cp Cargo.toml mykcov1:/volume
 docker cp crates/ mykcov1:/volume
 
-docker exec -t mykcov1 /bin/bash -c "cargo kcov --all" || echo "Failed generating report"
+docker exec -t mykcov1 /bin/sh -c "cargo check --all" || echo "Failed to generate rustc meta"
+docker exec -t mykcov1 /bin/sh -c "cargo kcov --all" || echo "Failed generating report"
 
 rm -R cov
 mkdir -p cov
@@ -18,4 +16,3 @@ docker cp mykcov1:/volume/target/cov ./
 
 # Force remove image
 docker rm -f mykcov1
-
